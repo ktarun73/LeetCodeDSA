2fa0afbbc2d7fd985b8e5ba8fcc832c89283ed07
Tarun Kumar <ktarun7318@gmail.com>
2022-06-15 14:19:46 +0530

    Time: 3 ms (49.50%), Space: 42.5 MB (32.20%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -4,27 +4,16 @@ class Solution {
         Stack<Character> s2=new Stack<>();
         
         for(char ch:s.toCharArray()){
-            if(ch!='#'){
-                s1.push(ch);
-            }
-            else{
-                if(!s1.isEmpty()){
-                    s1.pop();
-                }
-            }
+            if(ch!='#') s1.push(ch);
+            else if(!s1.isEmpty() && ch=='#') s1.pop();
+            
         }
         
         for(char ch:t.toCharArray()){
-            if(ch!='#'){
-                s2.push(ch);
-            }
-            else{
-                if(!s2.isEmpty()){
-                    s2.pop();
-                }
-                
-            }
+            if(ch!='#') s2.push(ch);
+            else if(!s2.isEmpty() && ch=='#') s2.pop();
         }
+        
         while(!s1.isEmpty() && !s2.isEmpty() && s1.peek()==s2.peek()){
             s1.pop();
             s2.pop();
