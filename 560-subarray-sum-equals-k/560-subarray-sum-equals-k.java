0feef8d204039da4651a222819c86dc41f4c6a71
Tarun Kumar <ktarun7318@gmail.com>
2022-07-13 09:01:52 +0530

    Time: 36 ms (69.48%), Space: 69.4 MB (5.76%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -4,6 +4,7 @@ class Solution {
         map.put(0,1);
         int sum=0;
         int count=0;
+        
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
             if(map.containsKey(sum-k)){
@@ -11,7 +12,7 @@ class Solution {
             }
             map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        
         return count;
+        
     }
 }
