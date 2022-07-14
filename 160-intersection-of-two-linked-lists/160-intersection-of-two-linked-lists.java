0e6d44e93ff7c94c7131b47eea8a0d69415cd131
Tarun Kumar <ktarun7318@gmail.com>
2022-07-14 09:21:45 +0530

    Time: 2 ms (56.17%), Space: 55.7 MB (22.84%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,42 +11,40 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int a=0;
-        int b=0;
+        int a_len=0 , b_len=0;
         ListNode cur1=headA , cur2=headB;
         
         while(cur1!=null){
-            a++;
+            a_len++;
             cur1=cur1.next;
         }
         
         while(cur2!=null){
-            b++;
+            b_len++;
             cur2=cur2.next;
         }
         
         cur1=headA;
         cur2=headB;
         
-        if(b>a){
-            int temp=b-a;
-            while(temp!=0){
-                temp--;
+        if(a_len<b_len){
+            int skip=b_len-a_len;
+            while(skip!=0){
                 cur2=cur2.next;
-            }
-        }
-        else{
-            int temp=a-b;
-            while(temp!=0){
-                temp--;
-                cur1=cur1.next;
+                skip--;
             }
         }
         
-        while(cur1!=null || cur2!=null){
-            if(cur1==cur2){
-                return cur1;
+        else{
+            int skip=a_len-b_len;
+            while(skip!=0){
+                cur1=cur1.next;
+                skip--;
             }
+        }
+        
+        while(cur1!=null && cur2!=null){
+            if(cur1==cur2) return cur1;
             cur1=cur1.next;
             cur2=cur2.next;
         }
