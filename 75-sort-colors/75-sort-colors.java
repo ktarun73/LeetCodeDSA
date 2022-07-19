c809b2a9935739022a0af4f5a98f4337cfc52347
Tarun Kumar <ktarun7318@gmail.com>
2022-07-19 23:01:32 +0530

    Time: 1 ms (41.42%), Space: 40.4 MB (98.01%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,17 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            for(int j=1;j<nums.length;j++){
+                if(nums[j]<nums[j-1]){
+                    swap(nums,j,j-1);
+                }
+            }
+        }
+    }
+    
+    void swap(int[] ar,int i,int j){
+        int temp=ar[i];
+        ar[i]=ar[j];
+        ar[j]=temp;
     }
 }
