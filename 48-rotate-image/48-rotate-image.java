8022591bd2618eaf782359d5acd7e4f781a699fc
Tarun Kumar <ktarun7318@gmail.com>
2022-07-23 20:44:46 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (5.40%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -6,16 +6,19 @@ class Solution {
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
-            }
+            } 
         }
         
-        
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j]=temp;
-            }
+        for(int[] row:matrix){
+            rotate(row);
+        }
+    }
+    
+    void rotate(int[] ar){
+        for(int i=0;i<ar.length/2;i++){
+            int temp=ar[i];
+            ar[i]=ar[ar.length-1-i];
+            ar[ar.length-1-i]=temp;
         }
     }
 }
