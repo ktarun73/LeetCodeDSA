c9daae00f850c49a86eed7454cab88818c4916e5
Tarun Kumar <ktarun7318@gmail.com>
2022-07-23 20:57:28 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (84.39%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -6,11 +6,8 @@ class Solution {
             row=new ArrayList<>();
             for(int j=0;j<=i;j++){
                 if(j==0 || j==i) row.add(1);
-                else{
-                    row.add(prev.get(j-1)+prev.get(j));
-                }
+                else row.add(prev.get(j-1)+prev.get(j));
             }
-            
             prev=row;
             res.add(row);
         }
