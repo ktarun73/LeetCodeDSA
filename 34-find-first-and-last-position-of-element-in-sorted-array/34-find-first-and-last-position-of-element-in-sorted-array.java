c547339d03cdf06174569e85178874ca85675ed2
Tarun Kumar <ktarun7318@gmail.com>
2022-08-03 07:51:43 +0530

    Time: 1 ms (40.30%), Space: 47.1 MB (68.03%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans[]=new int[2];
+        int[] ans=new int[2];
         ans[0]=binarySearch(nums,target,true);
         ans[1]=binarySearch(nums,target,false);
         return ans;
     }
     
-    public int binarySearch(int[] nums,int target,boolean isStart){
+    int binarySearch(int[] nums, int target,boolean isStart){
         int ans=-1;
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int pivot=start+(end-start)/2;
-            if(nums[pivot]>target){
-                end=pivot-1;
-            }
-            else if(nums[pivot]<target){
-                start=pivot+1;
-            }
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]<target) i=mid+1;
+            else if(nums[mid]>target) j=mid-1;
             else{
-                ans=pivot;
+                ans=mid;
                 if(isStart){
-                    end=pivot-1;
+                    j=mid-1;
                 }
                 else{
-                    start=pivot+1;
+                  i=mid+1;
                 }
             }
         }
