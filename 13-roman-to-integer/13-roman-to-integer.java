67cfaded8728462b2256dc9deb8f9e867817c0dd
Tarun Kumar <ktarun7318@gmail.com>
2022-08-15 11:25:50 +0530

    Time: 9 ms (58.60%), Space: 47 MB (26.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,51 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        char[] ar=s.toCharArray();
-        int ans=0;
-        for(int i=ar.length-1;i>=0;i--){
-            if(i-1>-1){
-                if(ar[i]=='V' && ar[i-1]=='I') {
-                    ans+=4;
-                    i--;
-                    continue;
-                }
-                else if(ar[i]=='X' && ar[i-1]=='I'){
-                    ans+=9;
-                    i--;
-                    continue;
-                }
-                else if(ar[i]=='L' && ar[i-1]=='X'){
-                    ans+=40;
-                    i--;
-                    continue;
-                }
-                else if(ar[i]=='C' && ar[i-1]=='X'){
-                    ans+=90;
-                    i--;
-                    continue;
-                }
-                else if(ar[i]=='D' && ar[i-1]=='C'){
-                    ans+=400;
-                    i--;
-                    continue;
-                }
-                else if(ar[i]=='M' && ar[i-1]=='C'){
-                    ans+=900;
-                    i--;
-                    continue;
-                }
-            }
-            if(i>-1){
-                if(ar[i]=='I') ans+=1;
-                else if(ar[i]=='V') ans+=5;
-                else if(ar[i]=='X') ans+=10;
-                else if(ar[i]=='L') ans+=50;
-                else if(ar[i]=='C') ans+=100;
-                else if(ar[i]=='D') ans+=500;
-                else if(ar[i]=='M') ans+=1000;
+        HashMap<Character , Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int ans=map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+             ans-=map.get(s.charAt(i));
+            } else{
+             ans+=map.get(s.charAt(i));
             }
         }
         return ans;
     }
-    
 }
